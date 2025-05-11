dc1deb4c86007d35099da6b924350bb9bf2dca85
CHANGCHENG LV <lv123456789up@outlook.com>
2025-05-11 11:08:32 +0800

    添加启动 ota

--- micropython/main.py
+++ micropython/main.py
@@ -1,1 +1,13 @@
 # 测试
+import time
+from machine import Pin
+
+lkey = Pin(5, Pin.IN, Pin.PULL_UP)
+rkey = Pin(7, Pin.IN, Pin.PULL_UP)
+
+while rkey.value() == 1:
+    time.sleep(0.01)
+ 
+#boot.py
+import ota
+ota.run()  # 调用 run 函数
